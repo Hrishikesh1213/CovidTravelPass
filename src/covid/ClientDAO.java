c95a450bca056a7f13b33361f662a41e0dd6766c
Hrishikesh1213 <63440095+Hrishikesh1213@users.noreply.github.com>
2020-12-03 13:44:58 +0530

    initial commit

--- src/covid/ClientDAO.java
+++ src/covid/ClientDAO.java
@@ -3,6 +3,6 @@ package covid;
 public interface ClientDAO {
 
 	public int insertClient(Client c);
-	 public Client getClient(String username,String password);
+	public Client getClient(String username,String password);
 	
 }
