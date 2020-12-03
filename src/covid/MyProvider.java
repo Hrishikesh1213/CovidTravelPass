31ee337b521e611a03792ad8635e8d2e98644a0b
Hrishikesh Reddy <63440095+Hrishikesh1213@users.noreply.github.com>
2020-12-03 13:56:17 +0530

    Update MyProvider.java

--- src/covid/MyProvider.java
+++ src/covid/MyProvider.java
@@ -2,8 +2,8 @@ package covid;
 
 public interface MyProvider {
 	
-	String url="jdbc:mysql://127.0.0.1:3306/hi";
-	String username="root";
-	String password="5422";
+	String url="*********";
+	String username="*******";
+	String password="**********";
 
 }
